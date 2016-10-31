a4e0ce916e77436352889cde1ab6dffdda87192b
Florian Kiemes <florian@fa-kiemes.de>
2016-10-31 14:32:59 +0100

    Added comments.

--- hdl/NeoPixel_top.vhd
+++ hdl/NeoPixel_top.vhd
@@ -19,19 +19,24 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity NeoPixel_top is
   port(
-    CLK_IN      : in  std_logic;
+    --System clock
+    CLK_IN        : in  std_logic;
     
-    RST_BTN_IN  : in  std_logic;
+    --Reset button, negative logic, needs synchronisation
+    RST_BTN_N_IN  : in  std_logic;
     
-    CS_IN       : in  std_logic;
-    SCLK_IN     : in  std_logic;
-    MOSI_IN     : in  std_logic;
-    MISO_OUT    : out std_logic;
+    --SPI-lines
+    CS_IN         : in  std_logic;
+    SCLK_IN       : in  std_logic;
+    MOSI_IN       : in  std_logic;
+    MISO_OUT      : out std_logic;
     
-    RXD_IN      : in  std_logic;
-    TXD_OUT     : out std_logic;
+    --UART-lines
+    RXD_IN        : in  std_logic;
+    TXD_OUT       : out std_logic;
     
-    PIXEL_OUT   : out std_logic
+    --Data signal to NeoPixel-LED
+    PIXEL_OUT     : out std_logic
   );
 end NeoPixel_top;
 
@@ -40,9 +45,11 @@ architecture RTL of NeoPixel_top is
 
   component UART_top is
     port(
+      --System clock and master reset
       CLK_IN      : in  std_logic;
       RST_IN      : in  std_logic;
       
+      --Bus interface
       RD_IN       : in  std_logic;
       WR_IN       : in  std_logic;
       
@@ -50,18 +57,22 @@ architecture RTL of NeoPixel_top is
       DATA_IN     : in  std_logic_vector(7 downto 0);
       DATA_OUT    : out std_logic_vector(7 downto 0);
       
+      --UART-lines
       RXD_IN      : in  std_logic;
       TXD_OUT     : out std_logic;
       
+      --Interrupt line
       INT_OUT     : out std_logic
     );
   end component;
   
   component SPI_top is
     port(
+      --System clock and master reset
       CLK_IN      : in  std_logic;
       RST_IN      : in  std_logic;
       
+      --Bus interface
       RD_IN       : in  std_logic;
       WR_IN       : in  std_logic;
       
@@ -69,17 +80,20 @@ architecture RTL of NeoPixel_top is
       DATA_IN     : in  std_logic_vector(7 downto 0);
       DATA_OUT    : out std_logic_vector(7 downto 0);
       
+      --SPI-lines
       CS_IN       : in  std_logic;
       SCLK_IN     : in  std_logic;
       MOSI_IN     : in  std_logic;
       MISO_OUT    : out std_logic;
       
+      --Interrupt line
       INT_OUT     : out std_logic
     );
   end component;
   
   component BIUI_top is
     port(
+      --Master bus interface
       M_RD_IN     : in  std_logic;
       M_WR_IN     : in  std_logic;
       
@@ -87,6 +101,7 @@ architecture RTL of NeoPixel_top is
       M_DATA_IN   : in  std_logic_vector(7 downto 0);
       M_DATA_OUT  : out std_logic_vector(7 downto 0);
       
+      --Bus interface for slave 0 (SPI)
       S0_RD_OUT   : out std_logic;
       S0_WR_OUT   : out std_logic;
   
@@ -94,6 +109,7 @@ architecture RTL of NeoPixel_top is
       S0_DATA_OUT : out std_logic_vector(7 downto 0);
       S0_DATA_in  : in  std_logic_vector(7 downto 0);
       
+      --Bus interface for slave 1 (UART)
       S1_RD_OUT   : out std_logic;
       S1_WR_OUT   : out std_logic;
   
@@ -105,9 +121,11 @@ architecture RTL of NeoPixel_top is
 
   component Controller_top is
     port(
+      --System clock and master reset
       CLK_IN      : in  std_logic;
       RST_IN      : in  std_logic;
       
+      --Bus interface to serial com ports
       A_RD_OUT    : out  std_logic;
       A_WR_OUT    : out  std_logic;
       
@@ -115,6 +133,7 @@ architecture RTL of NeoPixel_top is
       A_DATA_OUT  : out std_logic_vector(7 downto 0);
       A_DATA_IN   : in  std_logic_vector(7 downto 0);
       
+      --Bus interface to MMU and WS-driver
       B_WR_OUT    : out std_logic;
       
       B_ADR_OUT   : out std_logic_vector(7 downto 0);
@@ -125,18 +144,21 @@ architecture RTL of NeoPixel_top is
 
   component BIUO_top is
     port(
+      --Master bus interface
       M_WR_IN     : in  std_logic;
       
       M_ADR_IN    : in  std_logic_vector(7 downto 0);
       M_DATA_IN   : in  std_logic_vector(7 downto 0);
       M_DATA_OUT  : out std_logic_vector(7 downto 0);
       
+      --Bus interface to slave 0 (MMU)
       S0_WR_OUT   : out std_logic;
   
       S0_ADR_OUT  : out std_logic_vector(7 downto 0);
       S0_DATA_OUT : out std_logic_vector(7 downto 0);
       S0_DATA_in  : in  std_logic_vector(7 downto 0);
       
+      --Bus interface to slave 1 (WS_Encoder)
       S1_WR_OUT   : out std_logic;
   
       S1_ADR_OUT  : out std_logic_vector(7 downto 0);
@@ -147,15 +169,18 @@ architecture RTL of NeoPixel_top is
 
   component MMU_top is
     port(
+      --System clock and master reset
       CLK_IN      : in  std_logic;
       RST_IN      : in  std_logic;
       
+      --Bus interface
       WR_IN       : in  std_logic;
       
       ADR_IN      : in  std_logic_vector(7 downto 0);
       DATA_IN     : in  std_logic_vector(7 downto 0);
       DATA_OUT    : out std_logic_vector(7 downto 0);
       
+      --WS_Encoder interface
       M_RD_IN     : in  std_logic;
       
       M_ADR_IN    : in  std_logic_vector(7 downto 0);
@@ -165,26 +190,32 @@ architecture RTL of NeoPixel_top is
 
   component WS_Encoder_top is
     port(
+      --System clock and master reset
       CLK_IN      : in  std_logic;
       RST_IN      : in  std_logic;
       
+      --Bus interface
       WR_IN       : in  std_logic;
       
       ADR_IN      : in  std_logic_vector(7 downto 0);
       DATA_IN     : in  std_logic_vector(7 downto 0);
       DATA_OUT    : out std_logic_vector(7 downto 0);
       
+      --MMU interface
       M_RD_OUT    : out std_logic;
       
       M_ADR_OUT   : out std_logic_vector(7 downto 0);
       M_DATA_IN   : in  std_logic_vector(7 downto 0);
-    
+      
+      --LED data out
       PIXEL_OUT   : out std_logic
     );
   end component;
 
+  --Master reset
   signal reset      : std_logic;
-    
+  
+  --UART-BIUI-lines
   signal UART_rd    : std_logic;
   signal UART_wr    : std_logic;
   signal UART_int   : std_logic;
@@ -192,6 +223,7 @@ architecture RTL of NeoPixel_top is
   signal UART_din   : std_logic_vector(7 downto 0);
   signal UART_dout  : std_logic_vector(7 downto 0);
   
+  --SPI-BIUI-lines
   signal SPI_rd     : std_logic;
   signal SPI_wr     : std_logic;
   signal SPI_int    : std_logic;
@@ -199,26 +231,31 @@ architecture RTL of NeoPixel_top is
   signal SPI_din    : std_logic_vector(7 downto 0);
   signal SPI_dout   : std_logic_vector(7 downto 0);
 
+  --BIUI-Controller-lines
   signal BIUI_rd    : std_logic;
   signal BIUI_wr    : std_logic;
   signal BIUI_adr   : std_logic_vector(7 downto 0);
   signal BIUI_din   : std_logic_vector(7 downto 0);
   signal BIUI_dout  : std_logic_vector(7 downto 0);
 
+  --BIUO-Controller-lines
   signal BIUO_wr    : std_logic;
   signal BIUO_adr   : std_logic_vector(7 downto 0);
   signal BIUO_din   : std_logic_vector(7 downto 0);
   signal BIUO_dout  : std_logic_vector(7 downto 0);
 
+  --MMU-BIUO-lines
   signal MMU_wr     : std_logic;
   signal MMU_adr    : std_logic_vector(7 downto 0);
   signal MMU_din    : std_logic_vector(7 downto 0);
   signal MMU_dout   : std_logic_vector(7 downto 0);
   
+  --MMU-WS_Encoder-lines
   signal MMU_WS_rd  : std_logic;
   signal MMU_WS_adr : std_logic_vector(7 downto 0);
   signal MMU_WS_dta : std_logic_vector(7 downto 0);
   
+  --WS-BIUO-lines
   signal WS_wr      : std_logic;
   signal WS_adr     : std_logic_vector(7 downto 0);
   signal WS_din     : std_logic_vector(7 downto 0);
@@ -226,8 +263,10 @@ architecture RTL of NeoPixel_top is
   
 begin
 
-  reset   <=  not RST_BTN_IN; --Todo: RST_BTN_IN einsynchronisieren
+  --Connects master reset to reset button.
+  reset   <=  not RST_BTN_N_IN; --Todo: RST_BTN_N_IN einsynchronisieren
   
+  --Instatiations of submodules
   UART: UART_top port map(
     CLK_IN    =>  CLK_IN,
     RST_IN    =>  reset,
